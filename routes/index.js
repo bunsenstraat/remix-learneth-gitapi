44fbfb5cb267c4f7e66d9a73d02e02db2f6ef56a
Filip Mertens <bunsenstraat@gmail.com>
2020-02-18 15:10:24 +0100

    add pull, redis storage

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,15 @@ const util = require('util')
 var router = express.Router();
 var pretty = require('express-prettify');
 const http = require('http');
+const https = require('https');
 const fse = require('fs-extra');
-var cors = require('cors')
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const redis = require("redis");
+const client = redis.createClient();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}) );
 
 router.use(pretty({
   query: 'pretty'
@@ -15,8 +22,32 @@ router.use(pretty({
 
 router.use(cors())
 
-router.get('/clone/:repo/:branch?', function (req, res, next) {
 
+router.post('/getfile',function(req,response,next){
+  let data;
+
+  
+  //console.log(req.body.description.file);
+
+  https.get(req.body.description.file, (res) => {
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
+    res.on('data', (d) => {
+      data = d
+      console.log(d);
+      response.write(d);
+      response.send();
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });
+
+  
+});
+
+
+router.get('/clone/:repo/:branch?', function (req, res, next) {
+  
   const path = '/tmp'
   if(typeof req.params=="undefined"){
     req.params.branch = `master`;
@@ -29,94 +60,132 @@ router.get('/clone/:repo/:branch?', function (req, res, next) {
     const id = uniqid();
     const url = `https://github.com/${req.params.repo}`;
     const rawpath = `https://raw.githubusercontent.com/${req.params.repo}/${req.params.branch}/`;
-    shell.cd(path)
-    const cmd = `git clone --single-branch --branch ${req.params.branch} ${url} ${id}`;
-    shell.exec(cmd, function (code, stdout, stderr) {
 
-      const tree = dirTree(path + "/" + id, {
-        exclude: /.git/,
-        extensions: /\.(md|sol|js)$/
-      });
-      if (tree == null) {
-        res.status(500).send('Repo is empty or does not exist')
-      } else {
-        const workshops =
-          tree.children // children are the directories with workshops
-          .map(element => ({
-            name: element.name, // name of the workshop dir
-            //type: element.type,
-            description: ((typeof element.children != "undefined") ? element.children : [])
-              .filter(file => (file.extension == '.md'))
-              .map(file => Promise.all([fse.readFile(file.path,"utf8").then( c => { return c } )]))
-              ,
-            steps: ((typeof element.children != "undefined") ? element.children : []) // steps subdirectories but only when not empty
-              .filter(file => (file.type == 'directory'))
-              .map(stepchild => ({
-                name: stepchild.name, // name of step directory 
-                //type: stepchild.type,
-                markdown: ((typeof stepchild.children != "undefined") ? stepchild.children : []) // go through files in step directory
-                  .filter(
-                    file => (file.extension == '.md')
-                  )
-                  .map(
-                    file => ({
-                      file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-                    })
-                  ).values().next().value,
-                test: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-                  .filter(
-                    file => (file.extension == '.sol')
-                  )
-                  .filter(
-                    file => (file.name.includes('_test'))
-                  )
-                  .map(
-                    file => ({
-                      file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-                    })
-                  ).values().next().value,
-                solidity: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-                  .filter(
-                    file => (file.extension == '.sol')
-                  )
-                  .filter(
-                    file => (!file.name.includes('_test'))
-                  )
-                  .map(
-                    file => ({
-                      file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-                    })
-                  ).values().next().value,
-                js: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-                  .filter(
-                    file => (file.extension == '.js')
-                  )
-                  .filter(
-                    file => (!file.name.includes('_test'))
-                  )
-                  .map(
-                    file => ({
-                      file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-                    })
-                  ).values().next().value,
-              }))
-          }));
-        //  let newObj = Object.fromEntries(workshops.map((el) => [uniqid(), el]));
-        let entities = Object.assign(...Object.keys(workshops).map(k => ({
-          [uniqid()]: workshops[k]
-        })));
-        let ids = Object.keys(entities).map((k) => k);
-        //console.log(util.inspect(workshops));
-        const rm = `rm -rf ${path}/${id}`;
-        shell.exec(rm);
-        res.json({
-          entities: entities,
-          ids: ids
+    client.get(`${req.params.repo}/${req.params.branch}`,function(e,v){
+      console.log(e,v,path,id);
+      let tree;
+      if(v!=null){
+        tree = dirTree(v, {
+          exclude: /.git/,
+          extensions: /\.(md|sol|js)$/
+        }); 
+        console.log(tree);       
+      }
+     
+      if(v!=null && tree!=null){
+        shell.cd(path) 
+        const cmd = `git pull`
+        shell.exec(cmd, function (code, stdout, stderr) {
+          console.log("just getting the tree",v);
+          const workshops = getTree(v, '', rawpath);
+          res.json(workshops);
+        });
+      }else{
+        console.log("cloning",`${path}/${id}`);
+        client.set(`${req.params.repo}/${req.params.branch}`,`${path}/${id}`);
+        shell.cd(path)    
+        const cmd = `git clone --single-branch --branch ${req.params.branch} ${url} ${id}`;
+        shell.exec(cmd, function (code, stdout, stderr) {   
+          const tree = dirTree(path + "/" + id, {
+            exclude: /.git/,
+            extensions: /\.(md|sol|js)$/
+          });
+          if (tree == null) {
+            res.status(404).send('Repo is empty or does not exist')
+          } else {
+            const workshops = getTree(path,id, rawpath);
+            res.json(workshops);
+          }
         });
       }
-    });
+    })
+
   }
 
 
 })
+
+const getTree = (path,id, rawpath)=>{
+
+  const tree = dirTree(path + "/" + id, {
+    exclude: /.git/,
+    extensions: /\.(md|sol|js)$/
+  });
+
+  const workshops =
+  tree.children // children are the directories with workshops
+  .map(element => ({
+    name: element.name, // name of the workshop dir
+    id: uniqid(),
+    //type: element.type,
+    description: ((typeof element.children != "undefined") ? element.children : [])
+      .filter(file => (file.extension == '.md'))
+      .map(file => ({
+        file: `${rawpath}${element.name}/${file.name}`
+      })).values().next().value
+      ,
+    steps: ((typeof element.children != "undefined") ? element.children : []) // steps subdirectories but only when not empty
+      .filter(file => (file.type == 'directory'))
+      .map(stepchild => ({
+        name: stepchild.name, // name of step directory 
+        //type: stepchild.type,
+        markdown: ((typeof stepchild.children != "undefined") ? stepchild.children : []) // go through files in step directory
+          .filter(
+            file => (file.extension == '.md')
+          )
+          .map(
+            file => ({
+              file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
+            })
+          ).values().next().value,
+        test: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
+          .filter(
+            file => (file.extension == '.sol')
+          )
+          .filter(
+            file => (file.name.includes('_test'))
+          )
+          .map(
+            file => ({
+              file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
+            })
+          ).values().next().value,
+        solidity: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
+          .filter(
+            file => (file.extension == '.sol')
+          )
+          .filter(
+            file => (!file.name.includes('_test'))
+          )
+          .map(
+            file => ({
+              file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
+            })
+          ).values().next().value,
+        js: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
+          .filter(
+            file => (file.extension == '.js')
+          )
+          .filter(
+            file => (!file.name.includes('_test'))
+          )
+          .map(
+            file => ({
+              file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
+            })
+          ).values().next().value,
+      }))
+  }));
+
+  let entities = Object.assign(...Object.keys(workshops).map(k => ({
+    [workshops[k].id]: workshops[k]
+  })));
+  let ids = Object.keys(entities).map((k) => k);
+
+  return {ids:ids, entities:entities};
+
+
+}
+
+
 module.exports = router;
