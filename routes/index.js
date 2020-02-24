c4764dd725428c24701db271e69690b6b1d3a5bf
Filip Mertens <bunsenstraat@gmail.com>
2020-02-24 13:08:27 +0100

    bugfix shell was stuck in dir after cloning

--- routes/index.js
+++ routes/index.js
@@ -1,197 +1,225 @@
-var express = require('express');
-const shell = require('shelljs');
+var express = require("express");
+const shell = require("shelljs");
 const dirTree = require("directory-tree");
-var uniqid = require('uniqid');
-const util = require('util')
+var uniqid = require("uniqid");
 var router = express.Router();
-var pretty = require('express-prettify');
-const http = require('http');
-const https = require('https');
-const fse = require('fs-extra');
-var cors = require('cors');
-const bodyParser = require('body-parser');
+var pretty = require("express-prettify");
+const https = require("https");
+var cors = require("cors");
+const bodyParser = require("body-parser");
 const redis = require("redis");
 const client = redis.createClient();
+const os = require("os");
+
+class repo {
+  constructor(){
+    this.path = "" // where on disk the repo is cloned
+    this.id = "" // the unique id for the repo
+    this.rawpath = "" // the raw path of github files
+    this.branch = "master"
+    this.url = "" // the repo url
+    this.name = ""
+    this.tmpdir = ""
+  }
+
+}
+
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}) );
-router.use(pretty({
-  query: 'pretty'
-}));
-router.use(cors())
-router.post('/getfile',function(req,response,next){
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(
+  pretty({
+    query: "pretty"
+  })
+);
+router.use(cors());
+router.post("/getfile", function(req, response, next) {
   let data;
-  https.get(req.body.file, (res) => {
-    res.on('data', (d) => {
-      data = d
-      console.log(d);
-      response.write(d);
-      response.send();
+  https
+    .get(req.body.file, res => {
+      res.on("data", d => {
+        data = d;
+        console.log(d);
+        response.write(d);
+        response.send();
+      });
+    })
+    .on("error", e => {
+      console.error(e);
     });
-  }).on('error', (e) => {
-    console.error(e);
+});
+
+router.get("/clone/:repo/:branch?", function(req, res, next) {
+
+  let myrepo = new repo();
+  myrepo.tmpdir = os.tmpdir();
+  myrepo.branch =
+    typeof req.params == `undefined` ? `master` : req.params.branch;
+  if (!shell.which("git")) {
+    res.status(500).send("Git not available");
+    return;
+  }
+
+  
+  myrepo.url = `https://github.com/${req.params.repo}`;
+  myrepo.rawpath = `https://raw.githubusercontent.com/${req.params.repo}/${req.params.branch}/`;
+  myrepo.name = req.params.repo;
+
+  // get the data from redis, it retuns a path
+  client.get(`${myrepo.name}/${myrepo.branch}`, function(
+    e,
+    pathInRedis
+  ) {
+    myrepo.path = pathInRedis;
+    console.log("path in redis ",myrepo.path)
+    let tree;
+    if (myrepo.path != null) {
+      // if there is data in redis, we check if the cloned repo exists
+      tree = dirTree(myrepo.path, {
+        exclude: /.git/,
+        extensions: /\.(md|sol|js)$/
+      });
+    }
+
+    // we have cloned data, get the tree
+    if (myrepo.path != null && tree != null) {
+      shell.cd(myrepo.path);
+      console.log(myrepo.path);
+      console.log(shell.pwd());
+      const cmd = `git pull`; // get the updates
+      shell.exec(cmd, function(code, stdout, stderr) {
+        console.log("just getting the tree", myrepo.path, stdout, stderr);
+        sendTreeToOutput(myrepo,res);
+      });
+    } else {
+      
+      myrepo.id = uniqid(); // assig new id to this repo
+      myrepo.path = `${myrepo.tmpdir}/${myrepo.id}`
+      console.log("cloning", myrepo.url);
+      console.log(shell.pwd());
+      const cmd = `git clone --single-branch --branch ${myrepo.branch} ${myrepo.url} ${myrepo.path}`;
+      console.log(cmd, myrepo.path);
+
+      shell.exec(cmd, function(code, stdout, stderr) {
+        const tree = dirTree(myrepo.path, {
+          exclude: /.git/,
+          extensions: /\.(md|sol|js)$/
+        });
+        if (tree == null) {
+          res.status(404).send("Repo is empty or does not exist");
+          return;
+        }
+        console.log("cloning is done");
+        sendTreeToOutput(myrepo,res);
+        client.set(`${myrepo.name}/${myrepo.branch}`, `${myrepo.path}`); // store in redis
+      });
+    }
   });
 });
 
-router.get('/test/',function(req,res,next){
-  res.write("testing");
-  res.send();
-  return;
-  res.write("testing2");
-});
+const sendTreeToOutput = (myrepo,res)=>{
+  console.log("build tree", myrepo.path);
+  const workshops = getTree(myrepo); // build the tree
+  const getDateCmd = `git log -1 --format=%cd`; // command to get the date of the last commit
+  shell.cd(`${myrepo.path}`);
+  shell.exec(getDateCmd, function(code, stdout, stderr) {
+    console.log(stderr, stdout, myrepo.path);
+    workshops.datemodified = stdout;
+    res.json(workshops);
+  });
+  shell.cd("/");// do this otherwise the shell gets stuck if dir gets deleted
+}
 
-router.get('/clone/:repo/:branch?', function (req, res, next) {
-  const path = '/tmp'
-  req.params.branch = (typeof req.params==`undefined`)?`master`:req.params.branch
-  if (!shell.which('git')) {
-    res.status(500).send('Git not available')
-  } else {
-    const id = uniqid();
-    const url = `https://github.com/${req.params.repo}`;
-    const rawpath = `https://raw.githubusercontent.com/${req.params.repo}/${req.params.branch}/`;
-
-    client.get(`${req.params.repo}/${req.params.branch}`,function(e,v){ // get the data from redis
-      console.log(e,v,path,id);
-      let tree;
-      if(v!=null){
-        tree = dirTree(v, {
-          exclude: /.git/,
-          extensions: /\.(md|sol|js)$/
-        }); 
-        console.log(tree);       
-      }
-     
-      if(v!=null && tree!=null){
-        shell.cd(v) 
-        console.log(v);
-        const cmd = `git pull`
-        shell.exec(cmd, function (code, stdout, stderr) {
-          console.log("just getting the tree",v,stdout,stderr);
-          const workshops = getTree(v, '', rawpath);
-          const getDateCmd = `git log -1 --format=%cd`;
-          shell.exec(getDateCmd, function (code, stdout, stderr) {
-            workshops.datemodified = stdout;
-            res.json(workshops);
-          });
-        });
-      }else{
-        console.log("cloning",`${path}/${id}`);
-        
-        shell.cd(path)    
-        const cmd = `git clone --single-branch --branch ${req.params.branch} ${url} ${id}`;
-        shell.exec(cmd, function (code, stdout, stderr) {   
-          const tree = dirTree(path + "/" + id, {
-            exclude: /.git/,
-            extensions: /\.(md|sol|js)$/
-          });
-          if (tree == null) {
-            res.status(404).send('Repo is empty or does not exist')
-          } else {
-            const workshops = getTree(path,id, rawpath);
-            client.set(`${req.params.repo}/${req.params.branch}`,`${path}/${id}`); // store in redis
-            const getDateCmd = `git log -1 --format=%cd`; // command to get the date of the last commit
-            shell.cd(`${path}/${id}`);
-            shell.exec(getDateCmd, function (code, stdout, stderr) {
-              console.log(stderr,stdout,path);
-              workshops.datemodified = stdout;
-              res.json(workshops);
-            });
-            
-          }
-        });
-      }
-    })
-  }
-})
-
-const getTree = (path,id, rawpath)=>{
-
-  const tree = dirTree(path + "/" + id, {
+const getTree = (myrepo) => {
+  const tree = dirTree(myrepo.path, {
     exclude: /.git/,
     extensions: /\.(md|sol|js|yml)$/
   });
 
-  const workshops =
-  tree.children // children are the directories with workshops
-  .filter(file => (file.type == 'directory'))
-  .map(element => ({
-    name: element.name, // name of the workshop dir
-    id: uniqid(),
-    //type: element.type,
-    description: ((typeof element.children != "undefined") ? element.children : [])
-      .filter(file => (file.extension == '.md'))
-      .map(file => ({
-        file: `${rawpath}${element.name}/${file.name}`
-      })).values().next().value
-      ,
-    metadata: ((typeof element.children != "undefined") ? element.children : [])
-      .filter(file => (file.name == 'config.yml'))
-      .map(file => ({
-        file: `${rawpath}${element.name}/${file.name}`
-      })).values().next().value
-      ,
-    steps: ((typeof element.children != "undefined") ? element.children : []) // steps subdirectories but only when not empty
-      .filter(file => (file.type == 'directory'))
-      .map(stepchild => ({
-        name: stepchild.name, // name of step directory 
-        //type: stepchild.type,
-        markdown: ((typeof stepchild.children != "undefined") ? stepchild.children : []) // go through files in step directory
-          .filter(
-            file => (file.extension == '.md')
-          )
-          .map(
-            file => ({
+  const rawpath = myrepo.rawpath;
+
+  const workshops = tree.children // children are the directories with workshops
+    .filter(file => file.type == "directory")
+    .map(element => ({
+      name: element.name, // name of the workshop dir
+      id: uniqid(),
+      //type: element.type,
+      description: (typeof element.children != "undefined"
+        ? element.children
+        : []
+      )
+        .filter(file => file.extension == ".md")
+        .map(file => ({
+          file: `${rawpath}${element.name}/${file.name}`
+        }))
+        .values()
+        .next().value,
+      metadata: (typeof element.children != "undefined" ? element.children : [])
+        .filter(file => file.name == "config.yml")
+        .map(file => ({
+          file: `${rawpath}${element.name}/${file.name}`
+        }))
+        .values()
+        .next().value,
+      steps: (typeof element.children != "undefined" ? element.children : []) // steps subdirectories but only when not empty
+        .filter(file => file.type == "directory")
+        .map(stepchild => ({
+          name: stepchild.name, // name of step directory
+          //type: stepchild.type,
+          markdown: (typeof stepchild.children != "undefined"
+            ? stepchild.children
+            : []
+          ) // go through files in step directory
+            .filter(file => file.extension == ".md")
+            .map(file => ({
               file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-            })
-          ).values().next().value,
-        test: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-          .filter(
-            file => (file.extension == '.sol')
+            }))
+            .values()
+            .next().value,
+          test: (typeof stepchild.children != "undefined"
+            ? stepchild.children
+            : []
           )
-          .filter(
-            file => (file.name.includes('_test'))
-          )
-          .map(
-            file => ({
+            .filter(file => file.extension == ".sol")
+            .filter(file => file.name.includes("_test"))
+            .map(file => ({
               file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-            })
-          ).values().next().value,
-        solidity: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-          .filter(
-            file => (file.extension == '.sol')
+            }))
+            .values()
+            .next().value,
+          solidity: (typeof stepchild.children != "undefined"
+            ? stepchild.children
+            : []
           )
-          .filter(
-            file => (!file.name.includes('_test'))
-          )
-          .map(
-            file => ({
+            .filter(file => file.extension == ".sol")
+            .filter(file => !file.name.includes("_test"))
+            .map(file => ({
               file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-            })
-          ).values().next().value,
-        js: ((typeof stepchild.children != "undefined") ? stepchild.children : [])
-          .filter(
-            file => (file.extension == '.js')
+            }))
+            .values()
+            .next().value,
+          js: (typeof stepchild.children != "undefined"
+            ? stepchild.children
+            : []
           )
-          .filter(
-            file => (!file.name.includes('_test'))
-          )
-          .map(
-            file => ({
+            .filter(file => file.extension == ".js")
+            .filter(file => !file.name.includes("_test"))
+            .map(file => ({
               file: `${rawpath}${element.name}/${stepchild.name}/${file.name}`
-            })
-          ).values().next().value,
-      }))
-  }));
+            }))
+            .values()
+            .next().value
+        }))
+    }));
 
-  let entities = Object.assign(...Object.keys(workshops).map(k => ({
-    [workshops[k].id]: workshops[k]
-  })));
-  let ids = Object.keys(entities).map((k) => k);
+  let entities = Object.assign(
+    ...Object.keys(workshops).map(k => ({
+      [workshops[k].id]: workshops[k]
+    }))
+  );
+  let ids = Object.keys(entities).map(k => k);
 
-  return {ids:ids, entities:entities};
-
-
-}
-
+  return { ids: ids, entities: entities };
+};
 
 module.exports = router;
